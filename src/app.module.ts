cf25524ee07cb96fbf9ae63c5675f90fea9ce788
Thr10 <359366380@qq.com>
2023-07-29 18:19:20 +0800

    change: 业务服务端不提供上传或者下载静态资源的服务了

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppService } from './app.service';
-import { diskStorage } from 'multer';
-import { extname, join } from 'path';
-import { MulterModule } from '@nestjs/platform-express';
 import { CookingController } from './cooking.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CookingEntity } from './cooking.entity';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
-      serveRoot: '/assets',
-    }),
-    MulterModule.register({
-      storage: diskStorage({
-        destination: join('./public'),
-        filename: (_, file, callback) => {
-          const fileName = `${
-            new Date().getTime() + extname(file.originalname)
-          }`;
-          return callback(null, fileName);
-        },
-      }),
-    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: '127.0.0.1',
--- src/cooking.controller.ts
+++ src/cooking.controller.ts
@@ -1,13 +1,15 @@
 import {
   Body,
   Controller,
+  Get,
+  Param,
   Post,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { MoreThan, Repository } from 'typeorm';
 import { CookingEntity } from './cooking.entity';
 import { CreateCookingDto } from './create-cooking.dto';
 
@@ -18,11 +20,43 @@ export class CookingController {
     private readonly repository: Repository<CookingEntity>,
   ) {}
   @Post('create')
-  @UseInterceptors(FileInterceptor('file'))
   async create(@UploadedFile() file, @Body() body: CreateCookingDto) {
     return await this.repository.save({
       ...body,
       icon_picture: file.filename,
+    });
+  }
+
+  @Get('getList')
+  async getAll() {
+    return await this.repository.find({
+      select: [
+        'uuid',
+        'name_en',
+        'name_zh',
+        'fish_needs_num',
+        'farm_needs_num',
+        'spices_needs_num',
+      ],
+    });
+  }
+
+  @Get('getDetail:uuid')
+  async getDetail(@Param('uuid') uuid) {
+    return await this.repository.find({
+      select: [
+        'uuid',
+        'name_en',
+        'name_zh',
+        'fish_needs_num',
+        'farm_needs_num',
+        'spices_needs_num',
+      ],
+      where: [
+        {
+          uuid,
+        },
+      ],
     });
   }
 }
